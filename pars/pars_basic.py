e087b95b5c21efa491b692106db295b2f486a969
whibi1 <104696288+whibi1@users.noreply.github.com>
2023-04-10 22:37:29 +0300

    Update pars_basic.py

--- pars/pars_basic.py
+++ pars/pars_basic.py
@@ -3,13 +3,26 @@ import xml.etree.ElementTree as ET
 tree= ET.parse("book.xml")
 root= tree.getroot()
 
-
+liste=[]
 for a in root:
-    print(f"{a.tag}:,{a.text}")
+    
+    liste.append(a.tag)
+    liste.append(a.text)
     for b in a:
-        print (f"{b.tag}:,{b.text}")
+        
+        liste.append(b.tag)
+        liste.append(b.text)
         for c in b:
-             print(f"{c.tag}:,{c.text}")
+             liste.append(c.tag)
+             liste.append(c.text)
+
+print(liste)
+with open("aa.txt","r+") as f:
+    for line in liste:
+        f.write(line+"\n")
+
+
+
 
 
 
